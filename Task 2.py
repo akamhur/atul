6a9e837d72dc971c0a52cbdcf17e8c6c7aa4b3d6
atul <atul@spotline.com>
2025-07-02 02:27:15 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+a=input("Enter your first name: ")
+
+b=input("Enter your second name: ")
+
+c= a+ " " +b
+
+print(f'Hello, {c}! welcome to python program')
+
---
+++ Task1.py
@@ -0,0 +1,27 @@
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+c=a+b
+print(f'Addition: {c}')
+
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+c=a-b
+print(f'Subtraction: {c}')
+
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+c=a*b
+print(f'Multiplication: {c}')
+
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+c=a/b
+print(f'Division: {c}')
